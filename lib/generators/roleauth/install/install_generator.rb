8a36bc2d2227af269858ba245dfd79244354eb74
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-21 11:13:47 -0400

    adds first three migration generators

--- lib/generators/roleauth/install/install_generator.rb
+++ lib/generators/roleauth/install/install_generator.rb
@@ -1,7 +1,17 @@
 module RoleAuthorization
     module Generators
         class InstallGenerator < Rails::Generators::Base
-            
+            def generate_permission_migration
+                generate "migration", "RoleAuthorization::Permission name model_name"
+            end
+
+            def generate_role_mgration
+                generate "migration", "RoleAuthorization::Role name scope_name"
+            end
+
+            def generate_role_perm_migration
+                generate "migration", "RoleAuthorization::RolePermission permission:references role:references"
+            end
         end
     end
 end
