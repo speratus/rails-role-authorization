178bfe4762ee7227b149ff014d5ca312be668cab
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-23 16:03:59 -0400

    fills out RolePermission model

--- app/models/RolePermission.rb
+++ app/models/RolePermission.rb
@@ -1,7 +1,8 @@
 module RoleAuthorization
     module Models
         class RolePermission < Base
-            
+            belongs_to :permission
+            belongs_to :role
         end
     end
 end
