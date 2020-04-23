f8a1f2acab4856bed51a9c1966031fb1e6628013
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-23 16:12:27 -0400

    fills out ModelGroup model

--- app/models/ModelGroup.rb
+++ app/models/ModelGroup.rb
@@ -1,7 +1,10 @@
 module RoleAuthorization
     module Models
         class ModelGroup < Base
-            
+            validates :group, :record, presence: true
+
+            belongs_to :record, polymorphic: true
+            belongs_to :group
         end
     end
 end
