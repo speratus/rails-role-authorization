792d153439a488f5971462348963d51ddb5791c1
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-23 16:03:17 -0400

    fills out role model

--- app/models/Role.rb
+++ app/models/Role.rb
@@ -1,7 +1,10 @@
 module RoleAuthorization
     module Models
         class Role < Base
-            
+            validates :name, :group, presence: true
+
+            has_many :role_permissions
+            has_many :permissions, through: :role_permissions
         end
     end
 end
