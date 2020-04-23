5c6fb0909b559a367126a593370f4313952930e1
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-23 16:49:51 -0400

    adds a validation requiring group names to be unique

--- app/models/Group.rb
+++ app/models/Group.rb
@@ -2,6 +2,7 @@ module RoleAuthorization
     module Models
         class Group < Base
             validates :name, :model_name, presence: true
+            validates :name, :uniqueness: true
         end
     end
 end
