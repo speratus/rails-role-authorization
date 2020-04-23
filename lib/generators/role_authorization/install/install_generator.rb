61792abd5d081c717ca13e887c2eb87cc4f6d827
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-23 16:48:02 -0400

    refactors generate calls to be in a single method. makes group names unique

--- lib/generators/role_authorization/install/install_generator.rb
+++ lib/generators/role_authorization/install/install_generator.rb
@@ -1,22 +1,11 @@
 module RoleAuthorization
     module Generators
         class InstallGenerator < Rails::Generators::Base
-            def generate_permission_migration
+            def generate_migrations
                 generate "migration", "CreateRoleAuthPermissions name model_name"
-            end
-
-            def generate_role_migration
                 generate "migration", "CreateRoleAuthRoles name role_auth_group:references"
-            end
-
-            def generate_role_perm_migration
                 generate "migration", "CreateRoleAuthRolePermission permission:references role_auth_role:references"
-            end
-
-            def generate_group_migration
-                generate "migration", "CreateRoleAuthGroup name model_name"
-            end
-            def generate_record_group_migration
+                generate "migration", "CreateRoleAuthGroup name:uniq model_name"
                 generate "migration", "CreateRoleAuthModelGroup record:references{polymorphic} role_auth_group:references"
             end
 
