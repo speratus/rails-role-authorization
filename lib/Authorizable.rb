0f69d08ce8c598fc1844331dbb4eee4ec52ae94c
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-24 10:45:22 -0400

    adds validations to included modules

--- lib/Authorizable.rb
+++ lib/Authorizable.rb
@@ -1,5 +1,8 @@
 module RoleAuthorization::Authorizable
     def self.included(base)
-        
+        base&.has_one :model_group, as: :record
+        base&.has_one :group, through: :model_group
+
+        base&.validates :group, presence: true
     end
 end
