b837a453afb8243dd289255037f68a837f04625a
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-24 10:56:34 -0400

    defines define_permission macro

--- lib/Authorizable.rb
+++ lib/Authorizable.rb
@@ -1,8 +1,16 @@
+include RoleAuthorization::Models
+
 module RoleAuthorization::Authorizable
     def self.included(base)
         base&.has_one :model_group, as: :record
         base&.has_one :group, through: :model_group
 
         base&.validates :group, presence: true
+
+        def base.define_permission(*args)
+            args.each do |p|
+                Permission.find_or_create_by(name: p, model_name: base.to_s)
+            end
+        end
     end
 end
