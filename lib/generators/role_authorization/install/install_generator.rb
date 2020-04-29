c1fab735e9c8714c55e574b2d581fca5a6825387
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-29 15:26:41 -0400

    adds missing join table for role members

--- lib/generators/role_authorization/install/install_generator.rb
+++ lib/generators/role_authorization/install/install_generator.rb
@@ -7,6 +7,7 @@ module RoleAuthorization
                 generate "migration", "CreateRoleAuthRolePermission permission:references role_auth_role:references"
                 generate "migration", "CreateRoleAuthScope name:uniq model_name"
                 generate "migration", "CreateRoleAuthModelScope record:references{polymorphic} role_auth_scope:references"
+                generate "migration", "CreateRoleAuthRoleMember member:references{plymorphic} role_auth_role:references"
             end
 
             def migrate
