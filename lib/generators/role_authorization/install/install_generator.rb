95f57bf227c98dd5cc6052f617c46370e6fc2eb2
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-29 15:35:12 -0400

    unifies migration names so that they are all singular

--- lib/generators/role_authorization/install/install_generator.rb
+++ lib/generators/role_authorization/install/install_generator.rb
@@ -2,8 +2,8 @@ module RoleAuthorization
     module Generators
         class InstallGenerator < Rails::Generators::Base
             def generate_migrations
-                generate "migration", "CreateRoleAuthPermissions name model_name"
-                generate "migration", "CreateRoleAuthRoles name role_auth_scope:references"
+                generate "migration", "CreateRoleAuthPermission name model_name"
+                generate "migration", "CreateRoleAuthRole name role_auth_scope:references"
                 generate "migration", "CreateRoleAuthRolePermission permission:references role_auth_role:references"
                 generate "migration", "CreateRoleAuthScope name:uniq model_name"
                 generate "migration", "CreateRoleAuthModelScope record:references{polymorphic} role_auth_scope:references"
