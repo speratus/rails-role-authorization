a2481e5f46db53e30328d83632c3a45e98dd6f80
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-29 15:17:09 -0400

    refactors Group model to Scope

--- app/models/Group.rb
+++ app/models/Group.rb
@@ -1,6 +1,6 @@
 module RoleAuthorization
     module Models
-        class Group < Base
+        class Scope < Base
             validates :name, :model_name, presence: true
             validates :name, :uniqueness: true
         end
