8c7a1d3d8e0eefbeb9255b6ffae3f84b529e9625
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-04-29 15:16:43 -0400

    refactors group to scope to avoid collision with Active Record method

--- lib/generators/role_authorization/install/install_generator.rb
+++ lib/generators/role_authorization/install/install_generator.rb
@@ -3,10 +3,10 @@ module RoleAuthorization
         class InstallGenerator < Rails::Generators::Base
             def generate_migrations
                 generate "migration", "CreateRoleAuthPermissions name model_name"
-                generate "migration", "CreateRoleAuthRoles name role_auth_group:references"
+                generate "migration", "CreateRoleAuthRoles name role_auth_scope:references"
                 generate "migration", "CreateRoleAuthRolePermission permission:references role_auth_role:references"
-                generate "migration", "CreateRoleAuthGroup name:uniq model_name"
-                generate "migration", "CreateRoleAuthModelGroup record:references{polymorphic} role_auth_group:references"
+                generate "migration", "CreateRoleAuthScope name:uniq model_name"
+                generate "migration", "CreateRoleAuthModelScope record:references{polymorphic} role_auth_scope:references"
             end
 
             def migrate
