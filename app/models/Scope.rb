079dbe84c6f6684c547183f6dbdcf4f9a343cb75
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-05-02 18:39:45 -0400

    fixes typo

--- app/models/Scope.rb
+++ app/models/Scope.rb
@@ -2,7 +2,7 @@ module RoleAuthorization
     module Models
         class Scope < Base
             validates :name, :model_name, presence: true
-            validates :name, :uniqueness: true
+            validates :name, uniqueness: true
         end
     end
 end
