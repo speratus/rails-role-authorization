39a7a9c1f61402e00bebd5cea3a6f8629141c77e
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-05-06 17:05:33 -0400

    adds missing association

--- app/models/Role.rb
+++ app/models/Role.rb
@@ -1,8 +1,9 @@
 module RoleAuthorization
     module Models
         class Role < ActiveRecord::Base
-            validates :name, :group, presence: true
+            validates :name, :scope, presence: true
 
+            belongs_to :scope
             has_many :role_permissions
             has_many :permissions, through: :role_permissions
         end
